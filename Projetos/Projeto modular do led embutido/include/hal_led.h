24223821164cce925512e49b2969f2fe302801c5
viniciuscaffeu <viniciuscaffeu@usp.br>
2025-05-02 02:15:16 -0300

    Add files via upload

---
+++ Projetos/Projeto modular do led embutido/include/hal_led.h
@@ -0,0 +1,7 @@
+#ifndef HAL_LED_H
+#define HAL_LED_H
+
+void hal_led_init();
+void hal_led_toggle();
+
+#endif
---
+++ Projetos/Projeto modular do led embutido/include/led_embutido.h
@@ -0,0 +1,9 @@
+#ifndef LED_EMBUTIDO_H
+#define LED_EMBUTIDO_H
+
+#include <stdbool.h>
+
+void led_embutido_init();
+void led_embutido_set(bool estado);
+
+#endif
